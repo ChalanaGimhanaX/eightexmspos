45693fbe11f5a3833a2bd90a93f56e1a9778da88
Chalana Gimhana <chalana@nimalmotors.lk>
2026-09-21 17:42:06 +0000

    feat(ui): add product catalog quick-pick tiles, category chips, instant search, and cloud sync button

--- apps/desktop/src/Enightx.Pos.Wpf/Views/BillingView.xaml.cs
+++ apps/desktop/src/Enightx.Pos.Wpf/Views/BillingView.xaml.cs
@@ -1,6 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Enightx.Pos.Domain;
 using Enightx.Pos.Wpf.ViewModels;
 
 namespace Enightx.Pos.Wpf.Views;
@@ -12,6 +13,14 @@ public partial class BillingView : UserControl
     public BillingView()
     {
         InitializeComponent();
+    }
+
+    private async void BillingView_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is BillingViewModel vm)
+        {
+            await vm.LoadCatalogAsync();
+        }
     }
 
     private async void AddItem_Click(object sender, RoutedEventArgs e)
@@ -29,6 +38,62 @@ public partial class BillingView : UserControl
         {
             await vm.AddItemByBarcodeAsync(BarcodeInputBox.Text);
             BarcodeInputBox.Focus();
+        }
+    }
+
+    private void ProductCard_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && btn.DataContext is Product product && DataContext is BillingViewModel vm)
+        {
+            vm.AddProductToCart(product);
+        }
+    }
+
+    private void CategoryFilter_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && btn.DataContext is Category cat && DataContext is BillingViewModel vm)
+        {
+            vm.SelectedCategory = cat;
+        }
+    }
+
+    private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (DataContext is BillingViewModel vm)
+        {
+            vm.SearchQuery = SearchBox.Text;
+        }
+    }
+
+    private void IncrementQty_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && btn.DataContext is CartItemViewModel item && DataContext is BillingViewModel vm)
+        {
+            vm.IncrementItem(item);
+        }
+    }
+
+    private void DecrementQty_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && btn.DataContext is CartItemViewModel item && DataContext is BillingViewModel vm)
+        {
+            vm.DecrementItem(item);
+        }
+    }
+
+    private void RemoveItem_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && btn.DataContext is CartItemViewModel item && DataContext is BillingViewModel vm)
+        {
+            vm.RemoveItem(item);
+        }
+    }
+
+    private async void SyncCatalog_Click(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is BillingViewModel vm)
+        {
+            await vm.SyncCatalogWithCloudAsync();
         }
     }
 
